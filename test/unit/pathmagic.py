74bb4d4338e780b547da93992dce2fa59bae5aaa
saarefin <saarefin@cisco.com>
2019-09-09 12:56:39 +0530

    Corrected pathmagic.py script

--- test/unit/pathmagic.py
+++ test/unit/pathmagic.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
-bp = os.path.dirname(os.path.dirname(os.path.realpath('.'))).split(os.sep)
+bp = os.path.realpath('.').split(os.sep)
 modpath = os.sep.join(bp + ['src'])
 sys.path.insert(0, modpath)
+
